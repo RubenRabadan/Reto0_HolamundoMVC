3bf7dedcc4ab2dce9100935e186bf6117800e9ab
Ruben <rubir@DESKTOP-4JP5PHE>
2020-09-24 09:04:54 +0200

    First commit

---
+++ src/mycompany/model/Model.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompany.model;
+
+/**
+ *  Contains model methods for being used by a controller.
+ * @author Ruben Rabadan Hierro
+ */
+public interface Model {
+    /**
+     * Get the greeting
+     * @return Return a String that is the greeting
+     */
+    public String getGreeting();
+}
---
+++ src/mycompany/model/ModelImplementation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mycompany.model;
+
+/**
+ * Model implementation where return the greeting.
+ * @author Ruben Rabadan Hierro
+ */
+public class ModelImplementation implements Model{
+/**
+ * Return the greeting
+ * @return a String that contains the greeting.
+ */
+    @Override
+    
+    public String getGreeting() {        
+    String greeting=null;
+    
+    return greeting;
+    }
+      
+            
+}
